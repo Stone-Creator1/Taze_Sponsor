ec7a39f7e8dc9ee4ef85ef88f632b9f6d9914daa
Stone-Creator1 <hajymyrat635@gmail.com>
2025-11-03 02:12:09 +0500

    Add files via upload

--- NewStoneSponsor.py
+++ NewStoneSponsor.py
@@ -14,7 +14,7 @@ from telegram.ext import (
 if not os.path.exists("vpn_files"):
     os.makedirs("vpn_files")
 
-TOKEN = "8238956091:AAE0GUZKQA6hbkvBLPolg0jhhe6viCU-vTc"
+TOKEN = "8238956091:AAEMOB0ErIeF7IJVK3d9HKri0PwrdngHHu4"
 adminler = {7194433458}
 
 kanallar = []               # Goşulmaly kanallar
